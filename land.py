e797ff17566c651cbd4e8b8ad1f4f4a022627752
Lance Johnson <lancejohnson728@gmail.com>
2019-10-26 11:55:18 -0400

    Create paginated urls

--- land.py
+++ land.py
@@ -4,6 +4,7 @@ import requests_cache
 from bs4 import BeautifulSoup
 # from multiprocessing import Pool
 from db import get_counties
+import math
 
 from pprint import pprint
 
@@ -16,7 +17,22 @@ SCRAPERAPI_URL = 'http://api.scraperapi.com'
 def get_num_of_results(first_page_soup):
     resultscount_list = first_page_soup.find(
         'span', {'class': 'resultscount'}).text.split('\xa0')
+    # Quest: why does this kind of soup yield characters like
+    # '\xa0' when I use soup.text?
     return int(resultscount_list[5])
+
+
+def gen_paginated_urls(first_page_soup, num_of_results, page_1_url):
+    paginated_urls = []
+    if num_of_results > 15:
+        num_of_pages = math.ceil(num_of_results / 15)
+        pagination_base_url = first_page_soup.find(
+            'a', {'rel': 'next'})['href'][:-1]
+        for i in range(2, num_of_pages + 1):
+            paginated_urls.append(f'{pagination_base_url}{i}')
+    else:
+        paginated_urls.append(page_1_url)
+    return paginated_urls
 
 
 def main():
@@ -26,9 +42,11 @@ def main():
             SCRAPERAPI_URL,
             {'api_key': SCRAPER_API_KEY, 'url': county['landwatchurl']}
         )
-        soup = BeautifulSoup(resp.content, 'html.parser')
-        num_of_results = get_num_of_results(soup)
-        pprint(num_of_results)
+        first_page_soup = BeautifulSoup(resp.content, 'html.parser')
+        num_of_results = get_num_of_results(first_page_soup)
+        paginated_urls = gen_paginated_urls(
+            first_page_soup, num_of_results, county['landwatchurl'])
+        pprint(paginated_urls)
 
 
 if __name__ == '__main__':
