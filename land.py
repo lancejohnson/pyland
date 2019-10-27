31a34fd9dd178e870f35d55503c6e880a2edd7f6
Lance Johnson <lancejohnson728@gmail.com>
2019-10-26 21:06:50 -0400

    Parse first two listing values

--- land.py
+++ land.py
@@ -8,8 +8,6 @@ import math
 import aiohttp
 import asyncio
 
-from pprint import pprint
-
 SCRAPER_API_KEY = os.environ.get('SCRAPER_API_KEY', '')
 SCRAPERAPI_URL = 'http://api.scraperapi.com'
 
@@ -61,6 +59,34 @@ async def get_serps_response(paginated_urls):
         return soups
 
 
+def listing_parser(listing_soup):
+    '''This takes the soup for an individual property listing and transforms
+    it into the following schema
+     '''
+    example_dict = {  # noqa: F841
+        'pid': 25009439,
+        'listing_url': 'https://www.landwatch.com/Coconino-County-Arizona-Land-for-sale/pid/25009439',  # noqa: E501
+        'city': 'Flagstaff',
+        'state': 'AZ',
+        'price': 2800000,
+        'acres': 160.00,
+        'description': 'JUST REDUCED $310,000! Absolutely beautiful 160 acre parcel completely surrounded by the Coconino National Forest within 2 miles of Flagstaff city limits. ... ',  # noqa: E501
+        'office_name': 'First United Realty, Inc.',
+        'office_url': 'https://www.landwatch.com/default.aspx?ct=r&type=146,157956',  # noqa: E501
+        'office_status': 'Signature Partner',
+        'date_first_seen': datetime(2019, 10, 26, 20, 31, 32, 651887),
+        'price_per_acre': 17500.00  # this field is calculated
+    }
+
+    listing_dict = {}
+    base_url = 'https://www.landwatch.com'
+    listing_dict['url'] = base_url + listing_soup.find('a')['href']
+    listing_dict['pid'] = int(listing_dict['url'][-7:])
+
+    import pdb
+    pdb.set_trace()
+
+
 def main():
     p = argparse.ArgumentParser()
     p.add_argument(
@@ -85,12 +111,14 @@ def main():
         num_of_results = get_num_of_results(first_page_soup)
         paginated_urls = gen_paginated_urls(
             first_page_soup, num_of_results, CON_LIMIT)
-        pprint(paginated_urls)
 
         soups = [first_page_soup]
         soups.extend(asyncio.run(get_serps_response(paginated_urls)))
 
-        print(len(soups))
+        for soup in soups:
+            listings_soup_list = soup.select('div.result')
+            for listing_soup in listings_soup_list:
+                listing_parser(listing_soup)
 
 
 if __name__ == '__main__':
